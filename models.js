3b3b0c10784fabafd1890ce0270bf2014e004972
mee213 <evans.marlo@gmail.com>
2018-05-14 15:42:11 -0500

    fixed serialize function for sales model

--- models.js
+++ models.js
@@ -47,6 +47,22 @@ const salesWeekSchema = mongoose.Schema({
 	satDate: {type: String, required: true}  		
 });
 
+// *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
+// allow us to define properties on our object that manipulate
+// properties that are stored in the database. Here we use it
+// to generate a human readable string based on the author object
+// we're storing in Mongo.
+salesWeekSchema.virtual('totalSales').get(function() {
+  const totalSales = 	sunSales + 
+  						monSales + 
+  						tueSales +
+  						wedSales +
+  						thuSales +
+  						friSales +
+  						satSales;
+  return totalSales;
+});
+
 // this is an *instance method* which will be available on all instances
 // of the model. This method will be used to return an object that only
 // exposes *some* of the fields we want from the underlying data
@@ -57,7 +73,11 @@ laborWeekSchema.methods.serialize = function() {
 
 salesWeekSchema.methods.serialize = function() {
 
-  return this; // right now it returns everything, can change later as needed
+  return {
+  	week_id: this.week_id,
+  	startDate: this.sunSales,
+  	totalSales: this.totalSales
+  }
 }
 
 const LaborWeek = mongoose.model('LaborWeek', laborWeekSchema);
