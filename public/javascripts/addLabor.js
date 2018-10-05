630b8c6fa21cb8fee7b5601a4b50093484b7126c
mee213 <evans.marlo@gmail.com>
2018-10-05 13:40:35 -0500

    converted addLabor form submission to use ajax and added error/success messaging

---
+++ public/javascripts/addLabor.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const LABORWEEKS_URL = '/laborWeeks';
+
+// const printMessage = (message) => {
+//     //$.'js-message').style.backgroundColor = '#ffcccc';
+//     $('js-message').html(`<p>${message}</p>`);
+// }
+
+const postDataToLaborWeeksAPI = (data) => {
+
+    console.log('postDataToLaborWeeksAPI ran');
+
+    const settings = {
+        url: `${LABORWEEKS_URL}`,
+        data: data,
+        success: () => {
+            const successMessage = 'Your data has been successfully added';
+            console.log(successMessage);
+            const $messageDiv = $('.js-message');
+            $messageDiv.removeClass('hidden');
+            $messageDiv.html(`<p>${successMessage}</p><button type="button" class="remove">X</button>`);
+            $messageDiv.css("background-color", "#ccffcc");
+            $(':input').val("");
+            $('button.remove').click( e => {
+                $messageDiv.toggleClass('hidden');
+            });
+        },
+        error: () => {
+            console.error('There has been an error in postDataToLaborWeeksAPI');
+            const errorMessage = "That didn't work. Your data has NOT been added";
+            console.log(errorMessage);
+            const $messageDiv = $('.js-message');
+            $messageDiv.removeClass('hidden');
+            $messageDiv.html(`<p>${errorMessage}</p><button type="button" class="remove">X</button>`);
+            $messageDiv.css("background-color", "#ffcccc");
+            $('button.remove').click( e => {
+                $messageDiv.toggleClass('hidden');
+            });
+            //$messageDiv.hide(1500);
+        }
+    };
+
+    $.post(settings);
+
+}
+
+function ready() {
+    
+    $('form').submit( event => {
+        event.preventDefault();
+        console.log('Submit button clicked');
+
+        var formDataArray = $(':input').serializeArray();
+
+        console.log(formDataArray);
+
+        // we have an array of objects, 
+        //var myArray = [ { name: 'a', value: 1 }, { name: 'b', value: 2 }, { name: 'c', value: 3 } ];
+        // we want an object like { a: 1, b: 2, c: 3 }
+
+        // accumulator is an object, we'll set it to an empty object
+        // initially, when we call the reducer
+
+        // currentValue is the value of the current array element. 
+        function reducer(accumulator, currentValue) {
+            if (currentValue.name == "week_id" || currentValue.name == "periodEndDate") {
+                accumulator[currentValue.name] = currentValue.value;
+            } else {
+                accumulator[currentValue.name] = parseFloat(currentValue.value);
+            }
+
+            return accumulator;
+        }
+
+        var formDataObj = formDataArray.reduce(reducer, {});
+
+        console.log(formDataObj);
+
+        postDataToLaborWeeksAPI(formDataObj);
+
+
+
+      /*
+      const queryTarget = $(event.currentTarget).find('.js-query');
+      const query = queryTarget.val();
+      getDataFromAPIs(query)
+        .then(doSomeD3)
+        .catch(err => console.error(err));
+      */
+    });
+}
+
+$(ready);
