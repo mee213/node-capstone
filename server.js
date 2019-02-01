2834d89eb5eebad72ed88a21fe2af2c9638d0aa3
Marlo Wendell <mee213@gmail.com>
2019-02-01 11:48:52 -0600

    add new relic

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const request = require('request');
+const newrelic = require('newrelic');
 
 // Mongoose internally uses a promise-like object,
 // but its better to make Mongoose use built in es6 promises
